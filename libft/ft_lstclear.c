7b651a5350ceb32940e598c3d018d83ecac71889
Eloy <148364802+elaugust-42@users.noreply.github.com>
2024-02-16 21:07:51 +0000

    Add files via upload

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfriedri <tfriedri@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/05 20:30:47 by tfriedri          #+#    #+#             */
+/*   Updated: 2022/04/21 09:56:45 by tfriedri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*next;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		next = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		(*lst) = next;
+	}
+}
