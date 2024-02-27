08aca2912af9f99a3d5e43cfb72144dc19d2ea9d
Eloy Araújo <eloyaraujo@Eloys-MacBook-Air.local>
2024-02-27 22:07:33 +0000

    push

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 
 int main (void)
 {
-    ft_printf("what is this %s %s | %d | %d\n", "shit", "here", 1, 2);
-    printf("what is this %s %s | %d | %d\n", "shit", "here", 1, 2);
+    int result;
+
+    result = ft_printf("what is this %s %s | %d | %d | %% \n", "shit", "here", 1, 2);
+    printf(">> %d\n", result);
+    result = printf("what is this %s %s | %d | %d | %%\n", "shit", "here", 1, 2);
+    printf(">> %d\n", result);
 }
